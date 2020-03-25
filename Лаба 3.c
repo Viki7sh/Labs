0239b57f9b5b53467c4a9987ac4c9b704d707fee
Viki7sh <62552986+Viki7sh@users.noreply.github.com>
2020-03-25 17:23:00 +0300

    Add files via upload

---
+++ Лаба 3.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+#include <conio.h>
+     
+ 
+int main()
+{
+    int** mas, i, j, n;
+    printf("The size of your latin square is ");
+    scanf_s("%d", &n);
+
+    mas = (int**)calloc(n, sizeof(int*));
+      for (i = 0; i < n; i++)
+        mas[i] = (int*)calloc(n, sizeof(int));
+
+    srand(time(NULL));
+    int num = rand() % n;
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (num >= n)
+            {
+                num = 0;
+            }
+            mas[i][j] = num;
+            num++;
+
+        }
+            num--;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            printf("%d ", mas[i][j]);
+            printf("\n");
+    }
+    return 0;
+
+}
